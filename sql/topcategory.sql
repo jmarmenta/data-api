43c5c94024ac530c96fb4f9309935dd37b44a53f
Johana Armenta <jmarmenta02@gmail.com>
2025-03-17 12:51:49 -0500

    refactor SQL query to include aliases for readability

--- sql/topcategory.sql
+++ sql/topcategory.sql
@@ -1,4 +1,4 @@
-SELECT category.name, COUNT(rental.rental_id), SUM(payment.amount)
+SELECT category.name, COUNT(rental.rental_id) AS "Times Rented", SUM(payment.amount) AS "Total Revenue"
 FROM category
 JOIN film_category ON category.category_id = film_category.category_id
 JOIN film ON film_category.film_id = film.film_id
